916f47d4fd9d7a6d8849fc78a53519a5ee64e0c9
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-04-08 13:21:45 -0700

    initial commit

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require('express');
+const express = require("express");
+const helmet = require("helmet");
+
+const userRouter = require("./users/userRouter.js");
 
 const server = express();
 
-server.get('/', (req, res) => {
+server.use(helmet());
+server.use(express.json());
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use("/api/users", logger, userRouter);
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} request to ${req.originalUrl}`);
+  next();
+}
 
 module.exports = server;
